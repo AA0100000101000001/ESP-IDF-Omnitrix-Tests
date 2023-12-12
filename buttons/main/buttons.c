da67aa51f761fe3bd7a2d4b565b9836809195230
AA0100000101000001 <gracedgold26@gmail.com>
2023-12-12 14:01:05 +0200

    Used EPS_LOGI Instead Of Printf

--- buttons/main/buttons.c
+++ buttons/main/buttons.c
@@ -4,18 +4,22 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_timer.h"
+#include "esp_log.h"
+
+static const char *TAG = "MAIN";
 
 #define BTN 5 			//Start button
 #define BTN_RIGHT 1 	//Encoder right
 #define BTN_LEFT 4 		//Encoder left
 #define BTN_SELECT 3 	//Encoder button
 
-#define DELAY_TIME 600
+#define DELAY_TIME 200
  
 volatile bool button_pressed = false;
 volatile bool right_button_pressed = false;
 volatile bool left_button_pressed = false;
 volatile bool select_button_pressed = false;
+volatile unsigned char cnt = 0;
 
 //variables to keep track of the timing of recent interrupts
 int64_t  button_time = 0;  
@@ -88,33 +92,31 @@ void button_config()
     gpio_pullup_en(BTN_SELECT);
     gpio_set_intr_type(BTN_SELECT, GPIO_INTR_POSEDGE);
     gpio_isr_handler_add(BTN_SELECT, select_btn_isr_handler, NULL);
-    printf("config complete\n");
+    
+	printf("config complete\n");
 }
 
 void app_main()
+{   
 
     button_config();
 
     while (1) {
         if (button_pressed) {
-			printf("*\n");
 			button_pressed = false;
-			printf("Button Pressed");
+			ESP_LOGI(TAG, "Button Pressed\n");
         }
 		else if (right_button_pressed) {
-			printf("*\n");
 			right_button_pressed = false;
-			printf("Right Button Pressed");
+			ESP_LOGI(TAG, "Right Button Pressed\n");
         }
 		else if (left_button_pressed) {
-			printf("*\n");
 			left_button_pressed = false;
-			printf("Left Button Pressed");
+			ESP_LOGI(TAG, "Left Button Pressed\n");
         }
 		else if (select_button_pressed) {
-			printf("*\n");
 			select_button_pressed = false;
-			printf("Select Button Pressed");
+			ESP_LOGI(TAG, "Select Button Pressed\n");
         }
         vTaskDelay(DELAY_TIME / portTICK_PERIOD_MS);
     }
